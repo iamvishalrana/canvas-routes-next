bfdc9fe857832dbe66e42b43b99bd06b9e57b2bc
Vishal Rana <vishalrana4049@gmail.com>
2026-05-31 03:59:29 -0400

    Remove replayIntegration from stale client config; reduce tracesSampleRate to 0.1; remove sendDefaultPii

--- instrumentation-client.js
+++ instrumentation-client.js
@@ -7,9 +7,8 @@ import * as Sentry from "@sentry/nextjs";
 Sentry.init({
   dsn: "https://b3a011e919cbc56969f1da6bc828dcc2@o4511482659471360.ingest.us.sentry.io/4511482660585472",
 
-  tracesSampleRate: 1,
+  tracesSampleRate: 0.1,
   enableLogs: true,
-  sendDefaultPii: true,
 });
 
 export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
--- sentry.edge.config.js
+++ sentry.edge.config.js
@@ -9,12 +9,7 @@ Sentry.init({
   dsn: "https://b3a011e919cbc56969f1da6bc828dcc2@o4511482659471360.ingest.us.sentry.io/4511482660585472",
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
+  tracesSampleRate: 0.1,
 
-  // Enable logs to be sent to Sentry
   enableLogs: true,
-
-  // Enable sending user PII (Personally Identifiable Information)
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
-  sendDefaultPii: true,
 });
