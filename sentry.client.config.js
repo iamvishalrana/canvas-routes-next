fe40d180fa5da7eb00888689fd0d5ce9ddf09e46
Vishal Rana <vishalrana4049@gmail.com>
2026-06-01 21:44:42 -0400

    Filter browser extension runtime.sendMessage errors from Sentry

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -4,4 +4,8 @@ Sentry.init({
   dsn: "https://b3a011e919cbc56969f1da6bc828dcc2@o4511482659471360.ingest.us.sentry.io/4511482660585472",
 
   tracesSampleRate: 0.1,
+
+  ignoreErrors: [
+    /runtime\.sendMessage/,
+  ],
 });
