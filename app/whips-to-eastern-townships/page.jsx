f66c3e82cdd6b78f2c5842a46539236b10d6beb1
Vishal Rana <vishalrana4049@gmail.com>
2026-06-30 14:54:36 -0400

    Preload all car photos before showing itinerary page content

--- app/whips-to-eastern-townships/page.jsx
+++ app/whips-to-eastern-townships/page.jsx
@@ -269,7 +269,7 @@ export default function EasternTownshipsPage() {
 
   return (
     <div style={{ minHeight: '100vh', background: '#F5F1EC', fontFamily: 'sans-serif', color: '#1a1a1a' }}>
-      <PageLoader images={['/wtet.png']} minMs={2000} />
+      <PageLoader images={['/wtet.png', ...PARTICIPANTS.filter(p => p.photo).map(p => p.photo)]} minMs={2000} />
 
       {/* Scroll indicator */}
       <button
