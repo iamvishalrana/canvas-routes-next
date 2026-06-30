3afc344d04333a2e00fc33bde6c2e8c9564f8fb5
Vishal Rana <vishalrana4049@gmail.com>
2026-06-29 21:33:37 -0400

    Complete WTET participant list with all 16 registrants and correct car details

--- app/whips-to-eastern-townships/page.jsx
+++ app/whips-to-eastern-townships/page.jsx
@@ -7,18 +7,22 @@ import PageLoader from '../../components/PageLoader'
 const PASSWORD = 'eastern'
 
 const PARTICIPANTS = [
-  { name: 'Alain Meunier',         car: 'Chevrolet Corvette C7 Grand Sport Convertible', photo: '/WTET/Alain-Meunier.jpg' },
-  { name: 'Alain Sahakian',        car: '2023 Toyota Supra',                          photo: '/WTET/Alain-Sahakian.jpeg' },
-  { name: 'Alex Boutin',           car: '2026 Audi RS6 Performance',                  photo: '/WTET/Alex-Boutin.jpeg' },
-  { name: 'Bernard Attard',        car: '1970 Dodge Challenger',                      photo: '/WTET/Bernard-Attard.jpeg' },
-  { name: 'Fred Lefebvre',         car: '2020 Audi RS3',                              photo: '/WTET/Fred-Lefebvre.jpeg' },
-  { name: 'Jean-Philippe Remon',   car: '2011 BMW 135i',                              photo: '/WTET/Jean-Philippe.png' },
-  { name: 'Jerry',                 car: null,                                         photo: null },
-  { name: 'Louis Guindon',         car: '2023 Genesis G70 3.3T',                      photo: '/WTET/Louis-Guindon.png' },
-  { name: 'Louis Philippe Mauger', car: '2020 BMW M2 Compétition',                   photo: '/WTET/Louis-Mauger.jpg' },
-  { name: 'Michel Robert',         car: '2008 Porsche Boxster',                       photo: '/WTET/Michel-Robert.jpeg' },
-  { name: 'Tanya Ghingold',        car: '2012 Porsche 718 Cayman S Black Edition',   photo: '/WTET/Tanya-Ghingold.png' },
-  { name: 'Yvon Maggi',            car: '2014 Porsche 911 Turbo S',                   photo: '/WTET/Yvon-Maggi.png' },
+  { name: 'Alain Meunier',          car: '2019 Chevrolet Corvette Grand Sport',          photo: '/WTET/Alain-Meunier.jpg' },
+  { name: 'Alain Sahakian',         car: '2023 Toyota Supra',                            photo: '/WTET/Alain-Sahakian.jpeg' },
+  { name: 'Alexandre Boutin',       car: '2026 Audi RS6 Performance',                    photo: '/WTET/Alex-Boutin.jpeg' },
+  { name: 'Bernard Attard',         car: '1973 Dodge Challenger',                        photo: '/WTET/Bernard-Attard.jpeg' },
+  { name: 'Elie Massuda',           car: '2006 Porsche Boxster',                         photo: null },
+  { name: 'Frederic Lefebvre',      car: '2020 Audi RS3',                                photo: '/WTET/Fred-Lefebvre.jpeg' },
+  { name: 'Jean-François Rouette',  car: '2021 Audi TTRS',                               photo: null },
+  { name: 'Jean-Philippe Remon',    car: '2011 BMW 135i',                                photo: '/WTET/Jean-Philippe.png' },
+  { name: 'Jerry',                  car: null,                                           photo: null },
+  { name: 'Louis Guindon',          car: '2023 Genesis G70 3.3T',                        photo: '/WTET/Louis-Guindon.png' },
+  { name: 'Louis Philippe Mauger',  car: '2020 BMW M2 Compétition',                     photo: '/WTET/Louis-Mauger.jpg' },
+  { name: 'Michel Robert',          car: '2008 Porsche Boxster',                         photo: '/WTET/Michel-Robert.jpeg' },
+  { name: 'Raphael Lacoste',        car: '2011 Ferrari 458 Italia',                      photo: null },
+  { name: 'Tanya Ghingold',         car: '2012 Porsche 718 Cayman S Black Edition',     photo: '/WTET/Tanya-Ghingold.png' },
+  { name: 'Tino Guerrier',          car: '2021 Mercedes-Benz C63 S Coupé',              photo: null },
+  { name: 'Yvon Maggi',             car: '2014 Porsche 911 Turbo S',                     photo: '/WTET/Yvon-Maggi.png' },
 ]
 
 const MAP_MARKERS = [
